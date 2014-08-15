9859c0db30293195f9f8b58337c50b9ed457d2da
David Dollar <david@dollar.io>
2014-08-15 11:27:59 -0400

    embed the GeoTrust Global CA cert
    when go binaries are cross-compiled they do not get the benefit of os-supplied root certificates

--- notifier.go
+++ notifier.go
@@ -2,7 +2,10 @@ package gobrake
 
 import (
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -15,14 +18,50 @@ import (
 	"github.com/mreiferson/go-httpclient"
 )
 
-var (
-	transport = &httpclient.Transport{
+var client *http.Client
+
+var GeoTrustGlobalCACert = `-----BEGIN CERTIFICATE-----
+MIIDVDCCAjygAwIBAgIDAjRWMA0GCSqGSIb3DQEBBQUAMEIxCzAJBgNVBAYTAlVT
+MRYwFAYDVQQKEw1HZW9UcnVzdCBJbmMuMRswGQYDVQQDExJHZW9UcnVzdCBHbG9i
+YWwgQ0EwHhcNMDIwNTIxMDQwMDAwWhcNMjIwNTIxMDQwMDAwWjBCMQswCQYDVQQG
+EwJVUzEWMBQGA1UEChMNR2VvVHJ1c3QgSW5jLjEbMBkGA1UEAxMSR2VvVHJ1c3Qg
+R2xvYmFsIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2swYYzD9
+9BcjGlZ+W988bDjkcbd4kdS8odhM+KhDtgPpTSEHCIjaWC9mOSm9BXiLnTjoBbdq
+fnGk5sRgprDvgOSJKA+eJdbtg/OtppHHmMlCGDUUna2YRpIuT8rxh0PBFpVXLVDv
+iS2Aelet8u5fa9IAjbkU+BQVNdnARqN7csiRv8lVK83Qlz6cJmTM386DGXHKTubU
+1XupGc1V3sjs0l44U+VcT4wt/lAjNvxm5suOpDkZALeVAjmRCw7+OC7RHQWa9k0+
+bw8HHa8sHo9gOeL6NlMTOdReJivbPagUvTLrGAMoUgRx5aszPeE4uwc2hGKceeoW
+MPRfwCvocWvk+QIDAQABo1MwUTAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTA
+ephojYn7qwVkDBF9qn1luMrMTjAfBgNVHSMEGDAWgBTAephojYn7qwVkDBF9qn1l
+uMrMTjANBgkqhkiG9w0BAQUFAAOCAQEANeMpauUvXVSOKVCUn5kaFOSPeCpilKIn
+Z57QzxpeR+nBsqTP3UEaBU6bS+5Kb1VSsyShNwrrZHYqLizz/Tt1kL/6cdjHPTfS
+tQWVYrmm3ok9Nns4d0iXrKYgjy6myQzCsplFAMfOEVEiIuCl6rYVSAlk6l5PdPcF
+PseKUgzbFbS9bZvlxrFUaKnjaZC2mqUPuLk/IH2uSrW4nOQdtqvmlKXBx4Ot2/Un
+hw4EbNX/3aBd7YdStysVAq45pmp06drE57xNNB6pXE0zX5IJL4hmXXeXxx12E6nV
+5fEWCRE11azbJHFwLJhWC9kXtNHjUStedejV0NxPNO3CBWaAocvmMw==
+-----END CERTIFICATE-----`
+
+func init() {
+	chain := BuildRootCertificate()
+	config := tls.Config{}
+	config.RootCAs = x509.NewCertPool()
+	for _, cert := range chain.Certificate {
+		xc, err := x509.ParseCertificate(cert)
+		if err != nil {
+			panic(err)
+		}
+		config.RootCAs.AddCert(xc)
+	}
+	config.BuildNameToCertificate()
+
+	transport := &httpclient.Transport{
 		ConnectTimeout:        1 * time.Second,
 		ResponseHeaderTimeout: 5 * time.Second,
 		RequestTimeout:        10 * time.Second,
+		TLSClientConfig:       &config,
 	}
 	client = &http.Client{Transport: transport}
-)
+}
 
 type Notifier struct {
 	Client      *http.Client
@@ -96,4 +135,19 @@ func (n *Notifier) SendNotice(notice *Notice) error {
 	}
 
 	return nil
+}
+
+func BuildRootCertificate() (cert tls.Certificate) {
+	certPEMBlock := []byte(GeoTrustGlobalCACert)
+	var certDERBlock *pem.Block
+	for {
+		certDERBlock, certPEMBlock = pem.Decode(certPEMBlock)
+		if certDERBlock == nil {
+			break
+		}
+		if certDERBlock.Type == "CERTIFICATE" {
+			cert.Certificate = append(cert.Certificate, certDERBlock.Bytes)
+		}
+	}
+	return
 }
