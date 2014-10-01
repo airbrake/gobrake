f18b27b076271b29c7ff8062ede4bf36b9c92ea3
Guillaume J. Charmes <guillaume@cloud.com>
2014-10-01 08:15:51 -0400

    remove glog

--- notifier.go
+++ notifier.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"runtime"
 	"time"
 
-	"github.com/golang/glog"
 	"github.com/mreiferson/go-httpclient"
 )
 
@@ -59,7 +59,7 @@ func (n *Notifier) SetContext(name, value string) {
 func (n *Notifier) Notify(e interface{}, req *http.Request) error {
 	notice := n.Notice(e, req, 3)
 	if err := n.SendNotice(notice); err != nil {
-		glog.Errorf("gobrake failed (%s) reporting error: %v", err, e)
+		log.Printf("gobrake failed (%s) reporting error: %v", err, e)
 		return err
 	}
 	return nil
