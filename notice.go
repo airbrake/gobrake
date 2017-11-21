e14f0257ae15ed85ae113ea9dd0274401c4a1c13
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-11-21 10:14:52 +0200

    Bump version

--- notice.go
+++ notice.go
@@ -18,7 +18,7 @@ func getDefaultContext() map[string]interface{} {
 		defaultContext = map[string]interface{}{
 			"notifier": map[string]interface{}{
 				"name":    "gobrake",
-				"version": "3.1.0",
+				"version": "3.4.0",
 				"url":     "https://github.com/airbrake/gobrake",
 			},
 
