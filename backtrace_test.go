9097ce88a8710917fe0233336affac0b5f1933e2
Anatolii Mihailenco <anmic.webdev@gmail.com>
2018-01-10 15:09:01 +0000

    Set context.component to package name.

---
+++ backtrace_test.go
@@ -0,0 +1,40 @@
+package gobrake
+
+import (
+	testpkg1 "github.com/airbrake/gobrake/internal/testpkg1"
+	testpkg2 "github.com/airbrake/gobrake/internal/testpkg2"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
+)
+
+var _ = Describe("backtraceFromErrorWithStackTrace", func() {
+	It("returns package name", func() {
+		var tests = []struct {
+			err         error
+			packageName string
+		}{{
+			err:         testpkg1.Foo(),
+			packageName: "github.com/airbrake/gobrake/internal/testpkg1",
+		}, {
+			err:         testpkg1.Bar(),
+			packageName: "github.com/airbrake/gobrake/internal/testpkg1",
+		}, {
+			err:         testpkg2.NewError(),
+			packageName: "github.com/airbrake/gobrake/internal/testpkg2",
+		}}
+
+		type stackTracer interface {
+			StackTrace() errors.StackTrace
+		}
+
+		for _, test := range tests {
+			v, ok := test.err.(stackTracer)
+
+			Expect(ok).To(BeTrue())
+			packageName, _ := backtraceFromErrorWithStackTrace(v)
+			Expect(packageName).To(Equal(test.packageName))
+		}
+	})
+})
