7bd717c3643cd10498eb8c4ee7455413aecc4200
Anatolii Mihailenco <anmic.webdev@gmail.com>
2018-10-02 12:58:02 +0000

    Fix getRouteName.

--- cmd/api/route_stat/middleware.go
+++ cmd/api/route_stat/middleware.go
@@ -9,27 +9,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var notifier *gobrake.Notifier
 var pathMap map[string]string
-var lock = sync.RWMutex{}
+var once sync.Once
 
 var env = flag.String("env", "development", "environment, e.g. development or production")
 var host = flag.String("host", "", "host")
 var projectId = flag.Int64("project_id", 0, "project ID")
 var projectKey = flag.String("project_key", "", "project key")
 
-func init() {
+func NewAirbrakeMiddleware(engine *gin.Engine) func(c *gin.Context) {
 	flag.Parse()
 
-	notifier = gobrake.NewNotifierWithOptions(&gobrake.NotifierOptions{
+	notifier := gobrake.NewNotifierWithOptions(&gobrake.NotifierOptions{
 		ProjectId:   *projectId,
 		ProjectKey:  *projectKey,
 		Host:        *host,
 		Environment: *env,
 	})
-}
 
-func NewAirbrakeMiddleware(engine *gin.Engine) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		start := time.Now()
 
@@ -45,24 +42,14 @@ func NewAirbrakeMiddleware(engine *gin.Engine) func(c *gin.Context) {
 func getRouteName(c *gin.Context, engine *gin.Engine) string {
 	extractRouteNames(engine)
 
-	lock.RLock()
-	defer lock.RUnlock()
-
 	return pathMap[c.HandlerName()]
 }
 
 func extractRouteNames(engine *gin.Engine) {
-	lock.RLock()
-	if pathMap != nil {
-		return
-	}
-	lock.RUnlock()
-
-	lock.Lock()
-	defer lock.Unlock()
-
-	pathMap = make(map[string]string)
-	for _, ri := range engine.Routes() {
-		pathMap[ri.Handler] = ri.Path
-	}
+	once.Do(func() {
+		pathMap = make(map[string]string)
+		for _, ri := range engine.Routes() {
+			pathMap[ri.Handler] = ri.Path
+		}
+	})
 }
