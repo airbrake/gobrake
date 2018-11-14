c5fe06fd18c2bef6ffe25baf4540ce7ffc330fa4
Anatolii Mihailenco <anmic.webdev@gmail.com>
2018-11-14 16:09:51 +0200

    Add BenchmarkNotifyRequest test.

--- bench_test.go
+++ bench_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/airbrake/gobrake"
 )
@@ -34,6 +35,36 @@ func BenchmarkSendNotice(b *testing.B) {
 			}
 			if id != "123" {
 				b.Fatalf("got %q, wanted 123", id)
+			}
+		}
+	})
+}
+
+func BenchmarkNotifyRequest(b *testing.B) {
+	notifier := gobrake.NewNotifierWithOptions(&gobrake.NotifierOptions{
+		ProjectId:  1,
+		ProjectKey: "",
+	})
+
+	tm, err := time.Parse(time.RFC3339, "2018-01-01T00:00:00Z")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	info := &gobrake.RequestInfo{
+		Method:     "GET",
+		Route:      "/api/v4/groups",
+		StatusCode: 200,
+		Start:      tm,
+		End:        tm.Add(123 * time.Millisecond),
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			err := notifier.NotifyRequest(info)
+			if err != nil {
+				b.Fatal(err)
 			}
 		}
 	})
