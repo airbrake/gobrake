23fc62aa85a636cca6cc368081ac14adea5d8182
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-07-06 12:44:25 +0300

    Add more tests

--- trace_test.go
+++ trace_test.go
@@ -77,6 +77,24 @@ var _ = Describe("trace", func() {
 		Expect(trace.groups["nested1"]).To(BeNumerically("==", 2*time.Millisecond))
 		Expect(trace.groups["other"]).To(BeNumerically("==", 0))
 	})
+
+	It("supports spans on same level", func() {
+		c := context.Background()
+		var trace trace
+
+		c, sp0 := trace.Start(c, "sp0")
+		fakeClock.Advance(time.Millisecond)
+		sp0.Finish()
+
+		c, sp1 := trace.Start(c, "sp1")
+		fakeClock.Advance(time.Millisecond)
+		sp1.Finish()
+
+		Expect(trace.groups["sp0"]).To(BeNumerically("==", 1*time.Millisecond))
+		Expect(trace.groups["sp1"]).To(BeNumerically("==", 1*time.Millisecond))
+		Expect(trace.groups["other"]).To(BeNumerically("==", 0))
+
+	})
 })
 
 var _ = Describe("RouteTrace", func() {
