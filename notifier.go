b429ebdd64230c4a2e7884bc1b07950ca7bf2f25
Kyrylo Silin <silin@kyrylo.org>
2020-07-20 10:27:37 +0800

    Release v4.1.2

--- notifier.go
+++ notifier.go
@@ -17,7 +17,7 @@ import (
 )
 
 const notifierName = "gobrake"
-const notifierVersion = "4.1.1"
+const notifierVersion = "4.1.2"
 const userAgent = notifierName + "/" + notifierVersion
 
 const waitTimeout = 5 * time.Second
