0dde8d028847ea2052d298fe5c0c3935ebce05b7
Kyrylo Silin <silin@kyrylo.org>
2020-09-01 14:06:33 +0800

    race_test: fix the "too many open files" message

--- race_test.go
+++ race_test.go
@@ -38,7 +38,7 @@ var _ = Describe("Notifier", func() {
 
 	It("is race free", func() {
 		var wg sync.WaitGroup
-		for i := 0; i < 1000; i++ {
+		for i := 0; i < 100; i++ {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
