f152a3907d7d8b3d5e7dff3a21c3fa05b3b15867
Robert B Gordon <rbg@airbrake.io>
2022-01-25 13:51:54 -0600

    Update after feedback

--- fiber/fiberbrake.go
+++ fiber/fiberbrake.go
@@ -15,9 +15,16 @@ func New(notifier *gobrake.Notifier) fiber.Handler {
 		if notifier == nil {
 			return errors.New("airbrake notifier not defined")
 		}
+
+		// Starts the timer.
 		_, metric := gobrake.NewRouteMetric(context.TODO(), c.Route().Method, c.Route().Path)
 		err := c.Next()
+
+		// capture the status code and resolved Route
 		metric.StatusCode = c.Response().StatusCode()
+		metric.Route = c.Route().Path
+
+		// Send to Airbrake
 		_ = notifier.Routes.Notify(context.TODO(), metric)
 		return err
 	}
